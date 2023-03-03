d53f658cb47fd791822e740857bc29561b41367c
Fasile Endalkachew <sonofendalkachew@gmail.com>
2023-03-03 13:51:03 +0300

    feat: sequelize migration config

---
+++ migrations/20230303100848-sales-migration.js
@@ -0,0 +1,45 @@
+'use strict';
+require('dotenv').config();
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(process.env.DB_NAME, {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      orderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      itemName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()'),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable(process.env.DB_NAME);
+  },
+};
